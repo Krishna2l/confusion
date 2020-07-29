8bce0e87bd79dcb35e6b6edbf8604b4350b8b1f3
Krishna2l <srikrishnan1947@gmail.com>
2020-07-29 18:47:33 +0530

    commit 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  
+render() {
   return (
     <div>
       <Navbar dark color="primary">
@@ -15,6 +25,7 @@ function App() {
       <Menu />
     </div>
   );
+}
 }
 
 export default App;
